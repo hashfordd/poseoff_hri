7157d80c78a5c39be759783114e8e52154bf8925
lachlanwitt13 <145976799+lachlanwitt13@users.noreply.github.com>
2026-08-22 15:19:44 +1000

    Implement loss tracking and plot for training
    Added loss tracking and visualization for training and validation phases.

--- models/train.py
+++ models/train.py
@@ -5,8 +5,12 @@ from Training.transformer import transformer
 import torch.optim.lr_scheduler as lr_scheduler
 from Training.loadDataset import dynamic_collate_fn
 from  torch.nn.utils import clip_grad_norm_
+import matplotlib.pyplot as plt
+import numpy as np
 
 def train(learning_rate, batch_size, epochs):
+    print("Running")
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     train_dataset = flowfusionDataset('Dataset_UCF101/sorted_flow_poses/train')
@@ -25,7 +29,7 @@ def train(learning_rate, batch_size, epochs):
         model.train()
 
         total_loss = 0
-
+        loss_arr = []
         for batch_x, batch_y in train_loader:
             batch_x, batch_y = batch_x.to(device), batch_y.to(device)
 
@@ -39,12 +43,17 @@ def train(learning_rate, batch_size, epochs):
             optimizer.step()
 
             total_loss += loss.item()
+
+
+        loss_arr.append(loss.item())
         print(f"Epoch {epoch}/{epochs}, Loss: {loss}")
+
 
         model.eval()
         correct = 0
         total = 0
         total_val_loss = 0 
+        val_loss_arr = []
         with torch.no_grad():
             for batch_x, batch_y in test_loader:
                 batch_x, batch_y = batch_x.to(device), batch_y.to(device)
@@ -59,13 +68,31 @@ def train(learning_rate, batch_size, epochs):
                 correct += (predicted == batch_y).sum().item()
 
                 avg_loss = total_val_loss/len(test_loader)
+                
+        val_loss_arr.append(avg_loss)
 
         print(f'Validation Loss: {avg_loss}')
         print(f"Test Accuracy after Epoch {epoch+1}: {100 * correct / total:.2f}%")
 
+    
+    loss_arr = np.array(loss_arr)
+    val_loss_arr = np.array(val_loss_arr)
+
     print("Training Complete")
+    plt.figure(figsize=(15, 5))
+    plt.plot(loss_arr, label = "Training Loss", )
+    plt.plot(val_loss_arr, label = "Validation Loss")
+
+    plt.title('Model Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.savefig('loss_curve.png')
+    plt.close()
+    
     torch.save(model.state_dict(), 'model_XYZ.pth')
                
 if __name__ == "__main__":
-    train(1e-4, 32, 20)
+    train(1e-4, 32, 1)
     
